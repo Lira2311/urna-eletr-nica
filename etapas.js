8b11042986c393a78f75b7ae22cbc6083e86690e
Adriano <lira23110806@gmail.com>
2021-04-18 12:51:23 -0300

    urna

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: "VEREADOR" ,
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 50000,
+                nome: "Gohan",
+                partido: "Sayajins",
+                fotos:[
+                {url:'', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 60000,
+                nome: "Trunks",
+                partido: "Sayajins",
+                fotos:[
+                {url:'', legenda:''}
+                ]
+            },
+            ]
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+                numero:10,
+                nome: "Freeza",
+                partido: "Mal",
+                vice: "Cooler",
+                fotos:[
+                    {url:'', legenda: 'Prefeito'},
+                    {url:'', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: 80,
+                nome: "Goku",
+                partido: 'Bem',
+                vice: "Vegeta",
+                fotos:[
+                    {url:'', legenda: 'Prefeito'},
+                    {url:'', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
